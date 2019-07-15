8b79f010680759a90bc0c80fa6ee1ba6b12f6f73
Dyllon <dyllon@gmail.com>
2019-07-15 10:49:10 -0300

    Move node modules directory into a constant for setting up module routes

--- routes/moduleRoutes.js
+++ routes/moduleRoutes.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+const NODE_MODULES_DIR = path.join(__dirname, '..', 'node_modules');
+
 // Setup the routes for anything needed from installed node modules.
 module.exports.setup = function(app) { 
     const jqueryHandler = function(req, res) {
-        let filepath = path.join(__dirname, '..', 'node_modules', 'jquery', 'dist', 'jquery.min.js');
+        let filepath = path.join(NODE_MODULES_DIR, 'jquery', 'dist', 'jquery.min.js');
         res.sendFile(filepath);
     };
 
